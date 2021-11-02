b1631d0597ad08bef345a58362262c903b83d9f6
unknown <jeremy.mcguirk@gmail.com>
2021-11-02 12:28:08 -0400

    established connection to db

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
+// this creates sequelize instance 
 const Sequelize = require('sequelize');
 
+// creates the connection to our database, pass in your MySQL information for username and password
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
